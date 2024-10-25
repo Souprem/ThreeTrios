9456df7cd862b4b26bec94442cbf648153fbcfa7
pathedge27 <hedgecock.p@northeastern.edu>
2024-10-24 20:59:26 -0400

    No more comment

--- src/ThreeTriosModel.java
+++ src/ThreeTriosModel.java
@@ -14,7 +14,6 @@ public class ThreeTriosModel implements TriosModel<ThreeTriosCard>{
   private List<ThreeTriosCard> handPlayer2;
   private int handSize;
   private int numCardCells;
-//hgggg
 
   @Override
   public void playCard(int cardIndex, int row, int col) {
